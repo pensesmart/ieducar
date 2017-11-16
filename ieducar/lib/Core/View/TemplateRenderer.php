e0ea37fe4d88dcf341d2c193220d0021ce9d582f
carmoIn <helcio@carmo.in>
2017-11-16 00:26:55 -0200

    Atualizando Twig para versão 2.0

--- ieducar/lib/Core/View/TemplateRenderer.php
+++ ieducar/lib/Core/View/TemplateRenderer.php
@@ -2,7 +2,6 @@
 
 // Twig's autoloader will take care of loading required classes
 require_once 'lib/vendor/autoload.php';
-Twig_Autoloader::register();
 
 class TemplateRenderer
 {
@@ -13,17 +12,17 @@ class TemplateRenderer
 	{
 		// Merge default options
 		// You may want to change these settings
-		
+
 		$cachePath = false;
-		
+
 		if($GLOBALS['coreExt']['Config']->app->template->twig_cache) {
 			$cacheTwigPath = PROJECT_ROOT . $GLOBALS['coreExt']['Config']->app->template->twig_cache;
-			
+
 			if (is_dir($cacheTwigPath) && is_writable($cacheTwigPath))
 				$cachePath = $cacheTwigPath;
 		}
-			
-		
+
+
 
 		$envOptions += array(
 			'debug' => false,
@@ -31,12 +30,12 @@ class TemplateRenderer
 			'cache' => $cachePath,
 			'strict_variables' => true,
 		);
-		
+
 		$templateDirs = array_merge(
 			array(PROJECT_ROOT . '/views'), // Base directory with all templates
 			$templateDirs
 		);
-		
+
 		$this->loader = new Twig_Loader_Filesystem($templateDirs);
 		$this->environment = new Twig_Environment($this->loader, $envOptions);
 	}
